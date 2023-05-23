0b7dc2aecdb739e913fc60706bfa482e2dd8f031
JOHANN AMAYA LOPEZ <40411821+JohannBulls@users.noreply.github.com>
2023-05-22 23:08:37 -0500

    login

--- js/scripts.js
+++ js/scripts.js
@@ -333,7 +333,7 @@
                 }
             }
         });
-	}
+	}    
 
     function cformSuccess() {
         $("#contactForm")[0].reset();
@@ -347,7 +347,20 @@
             $(this).removeClass();
         });
 	}
-
+    $(document).ready(function() {
+        $("#show_hide_password a").on('click', function(event) {
+            event.preventDefault();
+            if($('#show_hide_password input').attr("type") == "text"){
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass( "fa-eye-slash" );
+                $('#show_hide_password i').removeClass( "fa-eye" );
+            }else if($('#show_hide_password input').attr("type") == "password"){
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass( "fa-eye-slash" );
+                $('#show_hide_password i').addClass( "fa-eye" );
+            }
+        });
+    });
     function csubmitMSG(valid, msg) {
         if (valid) {
             var msgClasses = "h3 text-center tada animated";
